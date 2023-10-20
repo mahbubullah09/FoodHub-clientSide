dea29506586d101b44ea38ec10f5430ef42e0076
Mahbubullah <mahbubullahpathan@gmail.com>
2023-10-21 04:48:11 +0600

    band ad

--- src/component/Home/BrandDetails.jsx
+++ src/component/Home/BrandDetails.jsx
@@ -1,18 +1,73 @@
-
+import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const BrandDetails = () => {
+  const { name } = useParams();
+  const data = useLoaderData();
 
-    const {name}  = useParams();
-    const data= useLoaderData();
+  const [brand, setBrand] = useState();
+  console.log(brand);
 
-    console.log(data,name);
-    return (
-        <div>
-            <h3>{name}</h3>
-            
+  useEffect(() => {
+    const findData = data?.find((brand) => brand.name == name);
+    setBrand(findData);
+  }, [name, data]);
+
+  console.log(data, name);
+  return (
+    <div>
+      <div className="mx-auto px-6   lg:block max-w-6xl  relative ">
+        <div className="carousel  ">
+          <div
+            id="slide1"
+            className="carousel-item relative w-full  bg-gray-700 "
+          >
+            <img src={brand?.slider1} className="lg:block w-full h-[70vh] " />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="text-white p-8  my-auto">
+                ❮
+              </a>
+
+              <a href="#slide2" className=" text-white p-8 my-auto">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div
+            id="slide2"
+            className="carousel-item relative w-full  bg-gray-700  "
+          >
+            <img src={brand?.slider2} className="lg:block w-full h-[70vh] " />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="text-white p-8 my-auto">
+                ❮
+              </a>
+              <div></div>
+
+              <a href="#slide3" className="text-white p-8 my-auto">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div
+            id="slide3"
+            className="carousel-item relative w-full  bg-gray-700"
+          >
+            <img src={brand?.slider3} className="lg:block w-full h-[70vh] " />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="text-white p-8 my-auto">
+                ❮
+              </a>
+
+              <a href="#slide1" className=" text-white p-8  my-auto">
+                ❯
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BrandDetails;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -11,7 +11,7 @@ let user;
   console.log(navigate);
 
   const navLink = (
-    <div className=" gap-4 flex ">
+    <div className=" gap-4 flex flex-col lg:flex-row ">
       <ul className="py-1">
         <NavLink
           to="/"
@@ -76,7 +76,7 @@ let user;
   return (
     <div>
       <div className="navbar Montserrat font-semibold  max-w-6xl mx-auto  ">
-        <div className="navbar-start">
+        <div className="navbar-start ">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
@@ -96,7 +96,7 @@ let user;
             </label>
             <ul
               tabIndex={0}
-              className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 "
             >
               {navLink}
             </ul>
